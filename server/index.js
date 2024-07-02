c3646df292720a5c02700a3ac67bef6e734afe22
John Nico M. Edisan <nicxsician@gmail.com>
2024-07-02 22:54:54 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,49 +1,49 @@
-import express from 'express'
-import { Server } from "socket.io"
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import { Server } from "socket.io";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 3500;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+    console.log(`listening on port ${PORT}`);
+});
 
 const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : [ "https://socket-io-chat-app-1-xakp.onrender.com", "http://localhost:5500", "http://127.0.0.1:5500"]
+        origin: process.env.NODE_ENV === "production" ? "https://socket-io-chat-app-1-xakp.onrender.com" : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
-})
+});
 
 io.on('connection', socket => {
-    console.log(`User ${socket.id} connected`)
+    console.log(`User ${socket.id} connected`);
 
     // Upon connection - only to user 
-    socket.emit('message', "Welcome to Chat App!")
+    socket.emit('message', "Welcome to Chat App!");
 
     // Upon connection - to all others 
-    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} connected`)
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`);
 
     // Listening for a message event 
     socket.on('message', data => {
-        console.log(data)
-        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
-    })
+        console.log(data);
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+    });
 
     // When user disconnects - to all others 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} disconnected`)
-    })
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`);
+    });
 
     // Listen for activity 
     socket.on('activity', (name) => {
-        socket.broadcast.emit('activity', name)
-    })
-})
+        socket.broadcast.emit('activity', name);
+    });
+});
