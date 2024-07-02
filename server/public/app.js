5577161d0c9b7f9ab3ce97994957e45c086f5fa8
John Nico M. Edisan <nicxsician@gmail.com>
2024-07-02 22:54:27 +0800

    Update app.js

--- server/public/app.js
+++ server/public/app.js
@@ -1,40 +1,38 @@
+const socket = io('https://socket-io-chat-app-1-xakp.onrender.com'); // Make sure this URL matches your Render.com deployment URL
 
-const socket = io('https://socket-io-chat-app-1-xakp.onrender.com')
-
-const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input')
+const activity = document.querySelector('.activity');
+const msgInput = document.querySelector('input');
 
 function sendMessage(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (msgInput.value) {
-        socket.emit('message', msgInput.value)
-        msgInput.value = ""
+        socket.emit('message', msgInput.value);
+        msgInput.value = "";
     }
-    msgInput.focus()
+    msgInput.focus();
 }
 
-document.querySelector('form')
-    .addEventListener('submit', sendMessage)
+document.querySelector('form').addEventListener('submit', sendMessage);
 
 // Listen for messages 
 socket.on("message", (data) => {
-    activity.textContent = ""
-    const li = document.createElement('li')
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+    activity.textContent = "";
+    const li = document.createElement('li');
+    li.textContent = data;
+    document.querySelector('ul').appendChild(li);
+});
 
 msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0, 5))
-})
+    socket.emit('activity', socket.id.substring(0, 5));
+});
 
-let activityTimer
+let activityTimer;
 socket.on("activity", (name) => {
-    activity.textContent = `${name} is typing...`
+    activity.textContent = `${name} is typing...`;
 
     // Clear after 3 seconds 
-    clearTimeout(activityTimer)
+    clearTimeout(activityTimer);
     activityTimer = setTimeout(() => {
-        activity.textContent = ""
-    }, 3000)
-})
+        activity.textContent = "";
+    }, 3000);
+});
